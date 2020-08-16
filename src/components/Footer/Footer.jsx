49f8ea78d12a170dc14e85c8c64a39539c579d02
AnjalBam <anjalbam81@gmail.com>
2020-08-16 19:04:26 +0545

    feat:[setup] Setup initial routes and folder-structure

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './footer.scss'
+
+
+const Footer = () => {
+    return (
+        <nav>
+            <h1>This is a Footer.</h1>
+        </nav>
+    )
+}
+
+export default Footer;
---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './navbar.scss'
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>This is a NavBar</h1>
+        </nav>
+    )
+}
+
+export default NavBar;
