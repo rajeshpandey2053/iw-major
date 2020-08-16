cd0206fb55dcad1f4718534bb0164cb35edd1be7
AnjalBam <anjalbam81@gmail.com>
2020-08-16 19:05:39 +0545

    feat:[setup] use the routes and components for basic rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import NavBar from "./components/Navbar/NavBar";
+import Footer from "./components/Footer/Footer";
+import Routes from "./components/Routes";
+import {BrowserRouter} from "react-router-dom";
+
 
 function App() {
-  return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <NavBar/>
+                <main>
+                    <Routes/>
+                </main>
+                <Footer/>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
