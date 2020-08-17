4e54d155bff09e76f7b3a968125b8abc8fed9d22
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-08-17 12:19:03 +0545

    feat: [create] component for login and web design complete

---
+++ src/components/Login/Login.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Login.scss';
+import {Link} from 'react-router-dom';
+import { Lock, AccountCircle, Facebook, GitHub } from '@material-ui/icons';
+import { ReactComponent as LoginImg } from './img/log.svg';
+
+
+const Login = () => {
+    return (
+        <div className="div-container">
+            <div className='form-container'>
+                <div className="login-register">
+                    <form action="" className="login-form">
+                        <h2 className="login-form__title">Sign In</h2>
+                        <div className="login-form__input-field">
+                            <AccountCircle />
+                            <input type="email" placeholder="Email" />
+                        </div>
+                        <div className="login-form__input-field">
+                            <Lock />
+                            <input type="password" placeholder="Password" />
+                        </div>
+                        <input type="submit" value="Login" className="btn solid" />
+                        <p className="social-text">or Login with social platforms</p>
+                        <div className="login-form__social-media-icons">
+                            <a href="#" className="social-icon"><Facebook /></a>
+                            <a href="#" className="social-icon"><GitHub /></a>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+            <div className="panel-container">
+                <div className="panel left-panel">
+                    <div className="content">
+                        <h3>HamroNotes</h3>
+                        <p>Difficult in Finding related Notes, Books...</p>
+                        <Link to="/register"><button className="btn transparent" id="register-button">Sign Up</button></Link>
+                    </div>
+                    <LoginImg className="image" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
