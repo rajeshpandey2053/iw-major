6395711ed99c10d22914a3e74e8f01cbd8c9579e
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-08-17 12:20:16 +0545

    feat: login url in nav

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import './navbar.scss'
+import './navbar.scss';
+import {Link} from 'react-router-dom';
 
 
 const NavBar = () => {
     return (
-        <nav>
-            <h1>This is a NavBar</h1>
-        </nav>
+        <div className="nav">
+            <div className="container">
+                <nav>
+                    <Link to='/'>Home</Link>
+                    <Link to='/login'>Login</Link>
+                </nav>
+            </div>
+        </div>
     )
 }
 
