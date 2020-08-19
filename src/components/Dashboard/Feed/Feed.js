6cf61754f866af7617999a94b47d1f1131530dd4
AnjalBam <anjalbam81@gmail.com>
2020-08-20 00:47:15 +0545

    feat[style]: post UI design

--- src/components/Dashboard/Feed/Feed.js
+++ src/components/Dashboard/Feed/Feed.js
@@ -1,19 +1,21 @@
 import React from "react";
 import './feed.scss';
+import Post from "./Post/Post";
 
 const Feed = () => {
     return (
         <div className='feed-wrapper'>
             <h1>This is the feed</h1>
-            <h1 className='test-scroll'>Testing for scroll.</h1>
-            <h1 className='test-scroll'>Testing for scroll.</h1>
-            <h1 className='test-scroll'>Testing for scroll.</h1>
-            <h1 className='test-scroll'>Testing for scroll.</h1>
-            <h1 className='test-scroll'>Testing for scroll.</h1>
-            <h1 className='test-scroll'>Testing for scroll.</h1>
-            <h1 className='test-scroll'>Testing for scroll.</h1>
-            <h1 className='test-scroll'>Testing for scroll.</h1>
-            <h1 className='test-scroll'>Testing for scroll.</h1>
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
         </div>
     )
 }
