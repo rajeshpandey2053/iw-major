4f81ea1bf91521afdd0b10352c3b2df78cb51989
AnjalBam <anjalbam81@gmail.com>
2020-08-20 00:55:18 +0545

    feat: create post component file created

---
+++ src/components/Dashboard/Feed/CreatePost/CreatePost.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './createPost.scss';
+
+
+const CreatePost = () => {
+    return (
+        <div>
+            <h1>Create Post Here.</h1>
+        </div>
+    )
+}
+
+export default CreatePost;
--- src/components/Dashboard/Feed/Feed.js
+++ src/components/Dashboard/Feed/Feed.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './feed.scss';
 import Post from "./Post/Post";
+import CreatePost from "./CreatePost/CreatePost";
 
 const Feed = () => {
     return (
         <div className='feed-wrapper'>
-            <h1>This is the feed</h1>
+            <CreatePost />
             <Post />
             <Post />
             <Post />
