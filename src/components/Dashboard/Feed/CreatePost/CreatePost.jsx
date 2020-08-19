756ad6e6788fc7555065c258f7868077a5f21b54
AnjalBam <anjalbam81@gmail.com>
2020-08-20 02:42:08 +0545

    feat: made createPost a form and made controlled elements and also created the footer in sidebar

--- src/components/Dashboard/Feed/CreatePost/CreatePost.jsx
+++ src/components/Dashboard/Feed/CreatePost/CreatePost.jsx
@@ -1,32 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
 import './createPost.scss';
 import blankAvatarImage from '../../../../images/blank-profile-picture-973460_1280.webp';
-import ImageRoundedIcon from '@material-ui/icons/ImageRounded';
+// import ImageRoundedIcon from '@material-ui/icons/ImageRounded';
 import AttachmentRoundedIcon from '@material-ui/icons/AttachmentRounded';
 
 const CreatePost = () => {
+    const [caption, setCaption] = useState('');
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = event => {
+        event.preventDefault()
+        // console.log('file', event.target.files[0]);
+        setFile(event.target.files[0]);
+    }
+
+    const handleInputChange = event => {
+        setCaption(event.target.value);
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log({caption, file})
+    }
+
     return (
-        <div className='create-post-wrapper'>
-            <div className='create-post-form'>
-                <div className='create-post-avatar'>
-                    <img src={blankAvatarImage} height={100} alt="Avatar"/>
-                </div>
-                <div className='input-section'>
-                    <textarea name="caption" id="caption" cols={30} rows={2} placeholder='Want to share something?' />
-                </div>
-            </div>
-
-            <div className="create-post-action-bar">
-                <div>
-                    <ImageRoundedIcon />
-                    <AttachmentRoundedIcon />
-                </div>
-                <div>
-                    <button type='button' className='post-button'>Post</button>
+        <form onSubmit={handleSubmit}>
+            <div className='create-post-wrapper'>
+                <div className='create-post-form'>
+                    <div className='create-post-avatar'>
+                        <img src={blankAvatarImage} height={100} alt="Avatar"/>
+                    </div>
+                    <div className='input-section'>
+                    <textarea name="caption" id="caption" cols={30}
+                              rows={2} placeholder='Want to share something?'
+                              onChange={handleInputChange}
+                              value={caption}
+                    />
+                    </div>
                 </div>
 
+                <div className="create-post-action-bar">
+                    <div>
+                        <div className="input-element">
+                            <label htmlFor="image"><AttachmentRoundedIcon/></label>
+                            <input type='file' alt='hello' id='image'
+                                   name='image' style={{display: 'none'}}
+                                   onChange={handleFileChange}
+                            />
+                        </div>
+
+
+                    </div>
+                    <div>
+                        <button type='submit' className='post-button'>Post</button>
+                    </div>
+                </div>
             </div>
-        </div>
+        </form>
     )
 }
 
