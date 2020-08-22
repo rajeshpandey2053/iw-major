83bfe1248946c94df4ecb766ff4f7f93f81cc599
AnjalBam <anjalbam81@gmail.com>
2020-08-23 00:34:18 +0545

    feat: conditionally rendered PostDetail

---
+++ src/components/Dashboard/PostDetail/PostDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './postDetail.scss'
+
+const PostDetail = () => {
+    return (
+        <div>
+            <h1>Hello i am a post detail...</h1>
+        </div>
+    )
+}
+export default PostDetail;
