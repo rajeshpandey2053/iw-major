1ac4b2ffb01c06a739338d4c41f604ad110b2975
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-08-26 21:08:42 +0545

    fix: del store to solve conflict

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createStore, applyMiddleware } from 'redux';
-import reducer from './loginReducer/loginReducer';
-import ReduxThunk from 'redux-thunk';
-
-const  store = createStore(reducer);
-console.log(store.getState());
-export default store;
