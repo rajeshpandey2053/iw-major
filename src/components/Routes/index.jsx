40eb37f0394d35216df50a7622204c3b136c46fb
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-08-26 22:41:02 +0545

    feat: route to profile page

--- src/components/Routes/index.jsx
+++ src/components/Routes/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {Switch, Route} from 'react-router-dom';
+import Profile from '../Profile/Profile';
+
 
 import Home from "../Home/Home";
 import Dashboard from "../Dashboard/Dashboard";
@@ -16,8 +18,9 @@ const Routes = (props) => {
                 <Route exact path='/dashboard' component={Dashboard} />
                 <Route exact path='/login' component={Login} props={props}/>
                 <Route exact path='/register' component={Register} />
+                <Route exact path='/profile' component={Profile} />
             </Switch>
         </React.Fragment>
     )
 }
-export default Routes
+export default Routes;
