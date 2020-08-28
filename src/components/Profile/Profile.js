44d65b41ab741a10fcd05bc5a34e5d621f3e9ebd
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-08-28 14:07:32 +0545

    feat: axios request to fetch user profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,33 +1,56 @@
-import React from 'react';
-import {connect} from 'react-redux';
-
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 import Sidebar from '../Dashboard/Sidebar';
 import ProfileView from './common/Profile';
 import { Person } from '@material-ui/icons';
+import { fetchProfiles } from '../../redux/actions/ProfileAction';
+
+
+const main_url = "http://127.0.0.1:8000/api/accounts/v1/user/profile"
 
 const Profile = (props) => {
-    
-    
-
+    const [userProfile, setUserProfile] = useState({});
+    useEffect(() => {
+        axios.get(main_url, {
+            headers: {
+                Authorization: `Token ${props.token}`,
+            },
+        })
+            .then((response) => {
+                setUserProfile(response.data);
+            })
+            .catch((error) => {
+                const errorMsg = error.message;
+                console.log(error);
+            });
+    },[])
     return (
         <div className="container dashboard-wrapper">
-      <div className="row dashboard-content-wrapper">
-          {/* Sidebar */}
-          <div id="sidebar" className='col-md-3 col-3'>
-              <Sidebar />
-          </div>
-          {/* Feed */}
-          <div id="feed" className="col-md-9 col-9">
-              <ProfileView />
-          </div>
-      </div>
-    </div>
+            <div className="row dashboard-content-wrapper">
+                {/* Sidebar */}
+                <div id="sidebar" className='col-md-3 col-3'>
+                    <Sidebar />
+                </div>
+                {/* Feed */}
+                <div id="feed" className="col-md-9 col-9">
+                    <ProfileView userProfile={userProfile} />
+                </div>
+            </div>
+        </div>
     )
 }
 
 const mapStateToProps = state => {
     return {
-        token: state.token
+        token: state.login.token,
+        profiles: state.profile.profiles
     }
 }
-export default connect(mapStateToProps)(Profile);
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchProfiles: (token) => dispatch(fetchProfiles(token))
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
