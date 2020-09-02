94f1179a458e066c8cae8eef588bb0f4811d233e
AnjalBam <anjalbam81@gmail.com>
2020-09-02 18:55:34 +0545

    feat: made dashboard reusable component and also fixed routes

---
+++ src/components/Dashboard/Explore/Explore.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./explore.scss";
+import Dashboard from "../Dashboard";
+
+const Explore = props => {
+  return (
+    <Dashboard>
+      <h1>Explore</h1>
+    </Dashboard>
+  );
+};
+
+export default Explore;
