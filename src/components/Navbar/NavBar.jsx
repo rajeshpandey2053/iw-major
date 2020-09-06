1a83561c07aad70384a517b37a31058e2eaa64a8
AnjalBam <anjalbam81@gmail.com>
2020-09-06 15:15:20 +0545

    feat: minor fixes

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,48 +1,57 @@
 import React from "react";
-import './navbar.scss';
-import { Link } from 'react-router-dom';
-import logo from '../../images/Logo143x32.svg';
-import { connect } from 'react-redux';
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+import logo from "../../images/Logo143x32.svg";
+import { connect } from "react-redux";
 
+const NavBar = props => {
+  return (
+    <div className="navbar">
+      <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top ">
+        <div className="container">
+          <Link className="navbar-brand logo-image" to="/">
+            <img src={logo} alt="alternative" />
+          </Link>
 
-const NavBar = (props) => {
-    return (
-        <div className="navbar">
-            <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-                <Link className="navbar-brand logo-image" to="/"><img src={logo} alt="alternative" /></Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-awesome fas fa-bars"></span>
-                    <span className="navbar-toggler-awesome fas fa-times"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link page-scroll" to="/">Home </Link>
-                        </li>
-                        {props?.token ?
-                            <li className="nav-item">
-                                <Link className="nav-link page-scroll" to="/logout">Logout</Link>
-                            </li> :
-                            <div className="no-login">
-                                <li className="nav-item">
-                                    <Link className="nav-link page-scroll" to="/login">Login</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link page-scroll" to="/register">Register</Link>
-                                </li>
-                            </div>
-                        }
-                    </ul>
+          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link page-scroll" to="/">
+                  Home{" "}
+                </Link>
+              </li>
+              {props?.token ? (
+                <li className="nav-item">
+                  <Link className="nav-link page-scroll" to="/logout">
+                    Logout
+                  </Link>
+                </li>
+              ) : (
+                <div className="no-login">
+                  <li className="nav-item">
+                    <Link className="nav-link page-scroll" to="/login">
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link page-scroll" to="/register">
+                      Register
+                    </Link>
+                  </li>
                 </div>
-            </nav>
+              )}
+            </ul>
+          </div>
         </div>
-    );
-}
+      </nav>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
-    return {
-        token: state.login.token
-    }
-}
+  return {
+    token: state.login.token,
+  };
+};
 
 export default connect(mapStateToProps)(NavBar);
